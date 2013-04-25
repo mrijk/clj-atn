c1911bad8f0d2e6aa5b245bff6aadc59a6bfa14c
Maurits <maurits.rijk@gmail.com>
2013-04-25 22:29:16 +0200

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-atn
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/clj_atn/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-atn.core-test
+  (:use clojure.test
+        clj-atn.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
