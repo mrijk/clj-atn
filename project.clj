1a77daa431d26c2d8462f96b7722c06b54e8f169
Maurits <maurits.rijk@gmail.com>
2013-04-26 23:13:59 +0200

    Add code to read binary files

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-atn "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Clojure code to parse Photoshop action files"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]])
--- src/clj_atn/core.clj
+++ src/clj_atn/core.clj
@@ -1,6 +1,10 @@
-(ns clj-atn.core)
+(ns clj-atn.core
+  (:require [clojure.java.io :as io]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn read-bin-file
+  "Read file as a byte array"
+  [filename]
+  (with-open [input (new java.io.FileInputStream filename) 
+              output (new java.io.ByteArrayOutputStream)]
+    (io/copy input output)
+    (.toByteArray output)))
