17b80c397b6d34f15b9744c58a4bc8376e850d29
Maurits <maurits.rijk@gmail.com>
2013-04-30 11:11:00 +0200

    Only read files we recognize

--- src/clj_atn/core.clj
+++ src/clj_atn/core.clj
@@ -1,4 +1,5 @@
 (ns clj-atn.core
+  (:import java.io.File)
   (:use [clj-atn util]))
 
 (defn- parse-field
@@ -14,15 +15,41 @@
 
 (def expanded (partial parse-field :expanded read-byte))
 
+(def nr-actions (partial parse-field :nr-actions read-int-32))
+
+(defn- actions [{:keys [stream tree]}]
+  {:stream stream
+   :tree (assoc tree :actions '())})
+
+(defn- valid-version? [version]
+  (or (= version 16) (= version 12)))
+
 (defn parse
   "Parse the binary array"
   [stream]
-  (-> {:stream stream :tree {}}
-      version
-      set-name
-      expanded))
+  (let [header (version {:stream stream :tree {}})]
+    (if (valid-version? (-> header :tree :version))
+      (->
+       header
+       set-name
+       expanded
+       nr-actions
+       actions))))
 
 (defn read-atn
   "Read Photoshop action file and return map"
   [filename]
   (-> filename read-bin-file parse :tree))
+
+(defn- atn-file?
+  [filename]
+  true)
+
+(defn- get-all-files [directory]
+  (map #(.getPath %) (filter #(.isFile %) (.listFiles (File. directory)))))
+
+(defn read-all-atns
+  "Read all Photoshop action files in a given directory"
+  [directory]
+  (map read-atn (filter atn-file? (get-all-files directory))))
+  
